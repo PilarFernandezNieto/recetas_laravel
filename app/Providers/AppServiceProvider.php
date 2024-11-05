77d84d244cf3e9a83ad513faf67886f0595bd883
Pilar Fernández Nieto <pilarfnieto@gmail.com>
2024-11-05 19:46:41 +0100

    Configuracion mail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verifica tu cuenta')
+                ->line('Tu cuenta ya está casi lista, solo tienes que pinchar en el siguiente enlace')
+                ->action('Confirmar Cuenta', $url)
+                ->line('Si no has creado esta cuenta puedes ignorar este mensaje');
+        });
     }
 }
